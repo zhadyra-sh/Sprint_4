6025ccd79896247fd63965fafbbc0bd41be500f5
zhadyra-sh <shzhadyra96@gmail.com>
2022-06-22 19:48:38 +0600

    first commit

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,15 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        String checkedName = "Zhadyra Shynybayeva";
+
+        Account account = new Account(checkedName);
+
+        boolean checkResult = account.checkNameToEmboss();
+
+        /*
+             В этом методе заложи логику работы с классом Account.
+             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+         */
+    }
+}
